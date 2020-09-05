d4db30b54d9f75767cb99d1425e3d915967dc596
Nikita <huskyray@mail.ru>
2020-09-06 00:43:21 +0300

    ex_1

---
+++ practice3/ex_1/ShapeTest.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class ShapeTest {
+    public static void startTest() {
+        Circle circle = new Circle();
+        Square square = new Square();
+        circle.showType();
+        square.showType();
+    }
+
+}
---
+++ practice3/ex_1/Square.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Square extends Shape {
+    @Override
+    public void showType() {
+        System.out.println("Square");
+    }
+}
